337e8aa8a3930e9a90cf4478ea6671e17d556da1
Sahara Rai <sxrai294@gmail.com>
2023-01-12 22:24:18 +0000

    first commit

---
+++ birthday/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ladipma's 5th birthday celebration</h1>
+        <p>
+        <strong>Day: </strong> Saturday, 28th January 5:30 onwards
+        <br></br>
+        <strong>Time: </strong> 5:30 onwards
+        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
