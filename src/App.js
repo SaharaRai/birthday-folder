26ceec7fc2e307cc8e93687120f2e5c6d4f9ebdc
Sahara Rai <sxrai294@gmail.com>
2023-01-14 16:09:34 +0000

    Guest list added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@
 // import photo from './LR_photo.svg';
 import photo from './photo_with_tocaboca_svg.svg';
 import './App.css';
-import { GiForkKnifeSpoon, GiBalloons, GiMusicalNotes } from 'react-icons/gi';
+import { GiForkKnifeSpoon, GiBalloons, GiMusicalNotes, GiPartyPopper } from 'react-icons/gi';
 import { BsMoonStarsFill } from 'react-icons/bs';
-
-
+import { FaChild } from 'react-icons/fa';
 
 function App() {
   return (
@@ -38,13 +37,13 @@ function App() {
           <p className='margin-top-0' style= {{color: '' }}>
             <strong>Day: </strong> Saturday, 28th January
             <br></br>
-            <strong>Time: </strong> 5:30 onwards
+            <strong>Time: </strong> 5:30 onwards 
+            <span style={{ color: 'purple' }}> <GiPartyPopper/></span>
             <br></br>
             <strong>Venue: </strong> Ladipma's home
           </p>
 
           <div style= {{backgroundColor: 'white', color: 'darkblue'}} className='padding-top-5 padding-bottom-5'>
-            
             <h3 className="margin-bottom-0" style= {{fontSize: '25px' }}> <span style={{ color: 'grey' }}><GiForkKnifeSpoon /></span> Menu:</h3>
             <p className='margin-top-0'>
               <strong>Starters: </strong> 
@@ -59,6 +58,24 @@ function App() {
             <h3 className="margin-bottom-0" style= {{fontSize: '25px' }}><GiMusicalNotes/> Entertainment Volunteers</h3>
             <p className='margin-top-0' style={{ paddingLeft: '10px', paddingRight: '10px'}}>
               We would love it if you are able to do any dance, singing or any other performance.
+            </p>
+          </div>
+
+          <div style= {{backgroundColor: 'white', color: 'darkblue'}} className='padding-top-5 padding-bottom-5'>
+            <h3 className="margin-bottom-0" style= {{fontSize: '25px' }}> <span style= {{color: 'orange' }}><FaChild /></span> Guest List:</h3>
+            <p className='margin-top-0 center'>
+              <span>- Bianka</span>
+              <br></br>
+              <span>- Ichya</span>
+              <br></br>
+              <span>- Leo</span>
+              <br></br>
+              <span>- Lobsang</span>
+              <br></br>
+              <span>- Ninambu</span>
+              <br></br>
+              <span>- Zilab</span>
+              <br></br>
             </p>
           </div>
         </div>
